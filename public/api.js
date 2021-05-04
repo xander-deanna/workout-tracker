2bccefcc5b8bc72faf0d5e1a8f8ff2e18a9cbc44
Deanna <xander.deanna@gmail.com>
2021-05-04 17:10:29 -0500

    added route to continue and existing workout

--- public/api.js
+++ public/api.js
@@ -1,4 +1,5 @@
 const API = {
+  // fetches and returns last workout 
   async getLastWorkout() {
     let res;
     try {
@@ -7,34 +8,33 @@ const API = {
       console.log(err)
     }
     const json = await res.json();
-
     return json[json.length - 1];
   },
+
+  // allows user to add more exercises to preexisting workout
   async addExercise(data) {
     const id = location.search.split("=")[1];
-
     const res = await fetch("/api/workouts/" + id, {
       method: "PUT",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data)
     });
-
     const json = await res.json();
-
     return json;
   },
+
+  // creates a new workout
   async createWorkout(data = {}) {
     const res = await fetch("/api/workouts", {
       method: "POST",
       body: JSON.stringify(data),
       headers: { "Content-Type": "application/json" }
     });
-
     const json = await res.json();
-
     return json;
   },
 
+  // ???
   async getWorkoutsInRange() {
     const res = await fetch(`/api/workouts/range`);
     const json = await res.json();
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,27 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
+// creates a new workout
 router.post("/api/workouts", ({ body }, res) => {
   Workout.create(body)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+// updates a workout
+router.put('/workouts/:id', async ({ params, body }, res) => {
+  Workout.findByIdAndUpdate(
+    params.id,
+    {
+      $push: {
+        exercises: body
+      }
+    }
+  )
     .then(dbWorkout => {
       res.json(dbWorkout);
     })
